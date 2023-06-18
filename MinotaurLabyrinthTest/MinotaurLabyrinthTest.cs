d74f250b2223aacda6cec1f0c900ddc59ab959fe
A00273725 <129540057+A00273725@users.noreply.github.com>
2023-06-18 05:01:16 +0000

    fixed tests

--- MinotaurLabyrinthTest/MinotaurLabyrinthTest.cs
+++ MinotaurLabyrinthTest/MinotaurLabyrinthTest.cs
@@ -6,9 +6,20 @@ namespace MinotaurLabyrinthTest
     public class Tests
     {
         [TestMethod]
-        public void DummyTest()
+        public void Activate_HeroWithSword_MonsterDefeatedAndHeroWins()
         {
-            Assert.AreNotSame(1, 2);
+          // Arrange
+            var nightmare = new Nightmare();
+            var hero = new Hero();
+            hero.HasSword = true;
+
+            // Act
+            nightmare.Activate(hero, null);
+
+            // Assert
+            Assert.AreEqual(0, nightmare.Health); // Nightmare should be defeated
+            Assert.AreEqual(100, hero.Health); // Hero's health should be restored to 100
+            Assert.IsTrue(hero.HasSword); // Hero should still have the sword
         }
     }
 }
