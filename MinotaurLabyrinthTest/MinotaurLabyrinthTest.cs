f61c1f911476008ceff15f06677fb7f9c8d56264
A00273725 <129540057+A00273725@users.noreply.github.com>
2023-06-18 07:58:32 +0000

    fixed formatting

--- MinotaurLabyrinthTest/MinotaurLabyrinthTest.cs
+++ MinotaurLabyrinthTest/MinotaurLabyrinthTest.cs
@@ -34,19 +34,22 @@ namespace MinotaurLabyrinthTest
             Assert.IsFalse(result); // DisplaySense should return false
         }
 
-        [TestMethod]
-         public void Activate_HeroWithoutSword_HeroDefeatedAndMonsterWins()
+       [TestMethod]
+        public void Activate_HeroWithoutSword_HeroDefeatedAndMonsterWins()
         {
             // Arrange
             var nightmare = new Nightmare();
-            var hero = new Hero(new Location(0,0));
+            var hero = new Hero(new Location(0, 0));
             hero.HasSword = false;
+
             // Act
             nightmare.Activate(hero, null);
+
             // Assert
             Assert.AreEqual(0, hero.Health); // Hero should be defeated
             Assert.IsTrue(nightmare.Health > 0); // Nightmare should have remaining health
             Assert.IsFalse(hero.HasSword); // Hero should still not have the sword
         }
+
     }
 }
