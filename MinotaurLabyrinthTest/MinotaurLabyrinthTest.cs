1d7c1f4f7b79feb1d1a5cadeef8e41a864396fa2
A00273725 <129540057+A00273725@users.noreply.github.com>
2023-06-18 07:37:28 +0000

    added more tests

--- MinotaurLabyrinthTest/MinotaurLabyrinthTest.cs
+++ MinotaurLabyrinthTest/MinotaurLabyrinthTest.cs
@@ -21,5 +21,37 @@ namespace MinotaurLabyrinthTest
             Assert.AreEqual(100, hero.Health); // Hero's health should be restored to 100
             Assert.IsTrue(hero.HasSword); // Hero should still have the sword
         }
+
+        [TestMethod]
+        public void DisplaySense_HeroDistanceGreaterThanOne_ReturnsFalse()
+        {
+            // Arrange
+            var nightmare = new Nightmare();
+            var hero = new Hero(new Location(0,0));
+            int heroDistance = 2;
+
+            // Act
+            bool result = nightmare.DisplaySense(hero, heroDistance);
+
+            // Assert
+            Assert.IsFalse(result); // DisplaySense should return false
+        }
+
+        [TestMethod]
+         public void Activate_HeroWithoutSword_HeroDefeatedAndMonsterWins()
+        {
+            // Arrange
+            var nightmare = new Nightmare();
+            var hero = new Hero(new Location(0,0));
+            hero.HasSword = false;
+
+            // Act
+            nightmare.Activate(hero, null);
+
+            // Assert
+            Assert.AreEqual(0, hero.Health); // Hero should be defeated
+            Assert.IsTrue(nightmare.Health > 0); // Nightmare should have remaining health
+            Assert.IsTrue(hero.HasSword); // Hero should still not have the sword
+        }
     }
 }
